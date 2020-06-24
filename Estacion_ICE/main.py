de02081921cda252c9bbfde81f1db582169b7b6d
Androx12 <andrey.r.q@hotmail.com>
2020-06-24 13:25:44 -0600

    Actualizacion

---
+++ Estacion_ICE/main.py
@@ -0,0 +1,6 @@
+from BLL.Ejecucion_Sistema_Control import Ejecucion_Sistema_Control
+
+if __name__ == '__main__':
+
+    Obj_EjecSistCont_BLL = Ejecucion_Sistema_Control
+    Obj_EjecSistCont_BLL.CorrerPrograma()
