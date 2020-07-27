f786fe9603342c23b52b3998ff57371816b6d4ae
Androx12 <andrey.r.q@hotmail.com>
2020-07-27 10:56:10 -0600

    Prueba

--- Estacion_ICE/main.py
+++ Estacion_ICE/main.py
@@ -3,5 +3,6 @@ from Proceso_Ejecucion import Ejecucion_Sistema_Control
 if __name__ == '__main__':
     Obj_EjecSistCont = Ejecucion_Sistema_Control()
     Obj_EjecSistCont.CorrerPrograma()
+    ObJ = {}
 
 
